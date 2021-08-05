d7c7c78310876a8431b8ced7c7ae121f13afc367
peter.chang <peter.chang@aplus-digital.com>
2021-08-05 11:34:15 +0800

    add patch stage commend

--- src/extension.ts
+++ src/extension.ts
@@ -1,21 +1,24 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+import { relative } from "path";
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('git-quick-patch-add.patch add', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from git-quick-patch-add!');
+	let disposable = vscode.commands.registerCommand('git-quick-patch-add.patch stage', (...resourceStates: vscode.SourceControlResourceState[]) => {
+		const workspace = vscode.workspace.workspaceFolders && vscode.workspace.workspaceFolders[0];
+		const resourceState = resourceStates[0];
+
+		if (resourceState && workspace) {
+			const newTerminal = vscode.window.createTerminal();
+			newTerminal.show();
+			const absFilePath = resourceState.resourceUri.path;
+			const filePath = relative(workspace.uri.path, absFilePath);
+			newTerminal.sendText(`git add -p ${filePath}`);
+		}
 	});
 
 	context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
+
+
+
