a417ad28070d51583acdf65a12e139371e38679d
peter.chang <peter.chang@aplus-digital.com>
2021-08-06 16:02:07 +0800

    refactor: replace get-path func by asRelativePath

--- src/copy-relative-path.ts
+++ src/copy-relative-path.ts
@@ -1,14 +1,7 @@
 import * as vscode from 'vscode';
-import { getPathFromWorkspace } from "./utils/get-path-from-workspace";
 
 export const copyRelativePath = (...resourceStates: vscode.SourceControlResourceState[]) => {
-    const workspace = vscode.workspace.workspaceFolders && vscode.workspace.workspaceFolders[0];
     const resourceState = resourceStates[0];
-
-    if (resourceState && workspace) {
-        const absFilePath = resourceState.resourceUri.path;
-        const filePath = getPathFromWorkspace(absFilePath, workspace);
-
-        vscode.env.clipboard.writeText(filePath);
-    }
+    const filePath = vscode.workspace.asRelativePath(resourceState.resourceUri);
+    vscode.env.clipboard.writeText(filePath);
 };
--- src/utils/get-path-from-workspace.ts
+++
@@ -1,6 +0,0 @@
-import { posix } from "path";
-import * as vscode from 'vscode';
-
-export const getPathFromWorkspace =  (absFilePath: string, workspace: vscode.WorkspaceFolder) => {
-    return posix.relative(workspace.uri.path, absFilePath);
-};
