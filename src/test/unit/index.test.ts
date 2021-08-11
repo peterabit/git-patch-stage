457f5b2cda1a9b9b3baecba239c7ab6b72b50d6b
peter.chang <peter.chang@aplus-digital.com>
2021-08-11 16:14:52 +0800

    add mocha unit test setting

---
+++ src/test/unit/index.test.ts
@@ -0,0 +1,8 @@
+import * as assert from 'assert';
+
+describe('Extension Test Suite', () => {
+	it('Sample test', () => {
+		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
+		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+	});
+});
